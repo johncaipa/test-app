f22eb97b5cf7fe0573d90c15c8cc45ae3f7def7b
siot-ingenieria <direccion@siot-ingenieria.com.co>
2023-05-06 12:29:11 -0500

    first commit

---
+++ assets/404View.f3743ab4.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./_plugin-vue_export-helper.cdc0426e.js";import{J as o,K as r}from"./index.ff394acc.js";const t={};function c(n,_){return o(),r("h1",null,"404 Page Not Found :(")}const f=e(t,[["render",c]]);export{f as default};

3ef5f84d1dd4423350f7ad30ff034118bbe9939d
siot-ingenieria <direccion@siot-ingenieria.com.co>
2023-05-29 15:09:51 -0500

    add chart and table

---
+++ assets/AboutView-90deadf1.js
@@ -0,0 +1,1 @@
+import{o as e,c as a,a as t,_ as o}from"./index-065efeaf.js";import"https://cdn.jsdelivr.net/npm/@xeokit/xeokit-sdk/dist/xeokit-sdk.es.min.js";const m={__name:"AboutView",setup(c){return(r,_)=>(e(),a("main",null,[t(o)]))}};export{m as default};
